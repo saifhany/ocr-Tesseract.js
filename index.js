36af02e842476b92fe16755b89a625f5143225ee
saif hany <saifhany898@gmail.com>
2021-12-05 13:44:22 +0200

    dd

--- index.js
+++ index.js
@@ -19,7 +19,7 @@ app.use(bodyParser.urlencoded({ extended: true }));
 app.use('/', Router);
 
 
-const PORT = 8000;
+const PORT = process.env.PORT || 8000;
 
 
 app.listen(PORT, () => console.log(`Server is running successfully on PORT ${PORT}`));
